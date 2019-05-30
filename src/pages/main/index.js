b334e196138e8266907425397ed7b31fc0140099
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-05-30 13:34:56 -0400

    Adding loading feature

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,14 +8,17 @@ import * as FavoriteActions from '../../store/action/favorites';
 class Main extends Component {
   static propTypes = {
     addFavoriteRequest: PropTypes.func.isRequired,
-    favorites: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        description: PropTypes.string,
-        url: PropTypes.string,
-      }),
-    ).isRequired,
+    favorites: PropTypes.shape({
+      loading: PropTypes.bool,
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          name: PropTypes.string,
+          description: PropTypes.string,
+          url: PropTypes.string,
+        }),
+      ),
+    }).isRequired,
   };
 
   state = {
@@ -29,6 +32,8 @@ class Main extends Component {
     const { repositoryInput } = this.state;
 
     addFavoriteRequest(repositoryInput);
+
+    this.setState({ repositoryInput: '' });
   };
 
   render() {
@@ -43,10 +48,12 @@ class Main extends Component {
             onChange={e => this.setState({ repositoryInput: e.target.value })}
           />
           <button type="submit">Adicionar</button>
+
+          {favorites.loading && <span>Carregando...</span>}
         </form>
 
         <ul>
-          {favorites.map(favorite => (
+          {favorites.data.map(favorite => (
             <li key={favorite.id}>
               <p>
                 <strong>{favorite.name}</strong> ({favorite.description})
